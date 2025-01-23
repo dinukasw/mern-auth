4684ca05ed2c792d0805abf75a43efed23ac1aaa
dinukasw <dinukasanthush50@gmail.com>
2025-01-23 15:49:42 +0530

    create  signup api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose
@@ -16,8 +20,12 @@ mongoose
         console.log(err);
     });
 
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000...");
 });
-
-app.use("/api/user", userRoutes);
