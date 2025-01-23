45bc1e85c13ffeaddcd109aa1da331cb992b0ec1
dinukasw <dinukasanthush50@gmail.com>
2025-01-23 13:40:34 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB...");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-
-app.listen(3000, ()=> {
-    console.log('Server is running on port 3000...');
-})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...");
+});
