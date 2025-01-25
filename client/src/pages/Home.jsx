c5188eac9a444ed141cc4f9efe1e8a2e57658110
dinukasw <dinukasanthush50@gmail.com>
2025-01-25 12:56:13 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,11 @@
 
 export default function Home() {
   return (
-    <div>
-      Homeeeeeeeeeee
+    <div className="max-w-lg mx-auto mt-10 flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-3xl font-bold ">Welcome to Auth App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi amet nemo soluta ab consectetur similique blanditiis earum architecto eveniet incidunt nulla est sequi inventore ad corrupti voluptates, quidem delectus vitae.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsum autem veniam excepturi praesentium voluptatem ea, amet obcaecati animi molestiae. Eaque maiores nesciunt deleniti eveniet voluptates placeat debitis, minus rem.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laborum blanditiis explicabo. Ipsam magni asperiores minus dicta adipisci ea quia, nihil, doloremque nemo illum aspernatur quod at. Expedita, eaque ipsum.</p>
     </div>
   )
 }
