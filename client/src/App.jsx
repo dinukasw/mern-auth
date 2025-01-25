6b931645fdf9e7802a73815944e13e2e020ab9bf
dinukasw <dinukasanthush50@gmail.com>
2025-01-25 16:42:57 +0530

    fixed small buges for deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
